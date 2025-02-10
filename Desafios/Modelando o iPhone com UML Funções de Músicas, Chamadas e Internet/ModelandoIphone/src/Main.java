5b1ce35e168b12cf820e54085efd7a34e0fba4b7
Gabriel <gabrielferreiradj2014@gmail.com>
2025-02-10 14:09:53 -0300

    ModelandoIPhone

---
+++ Desafios/Modelando o iPhone com UML Funções de Músicas, Chamadas e Internet/ModelandoIphone/src/Main.java
@@ -0,0 +1,21 @@
+// Exemplo de uso
+public class Main {
+    public static void main(String[] args) {
+        iPhone meuIphone = new iPhone();
+
+        // Testando Reprodutor Musical
+        meuIphone.tocar();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+
+        // Testando Aparelho Telefônico
+        meuIphone.ligar("1234-5678");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando Navegador na Internet
+        meuIphone.exibirPagina("https://www.google.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
---
+++ Desafios/Modelando o iPhone com UML Funções de Músicas, Chamadas e Internet/ModelandoIphone/src/ReprodutorMusical.java
@@ -0,0 +1,8 @@
+// Interface ReprodutorMusical
+public interface ReprodutorMusical {
+    void tocar();
+
+    void pausar();
+
+    void selecionarMusica(String musica);
+}
